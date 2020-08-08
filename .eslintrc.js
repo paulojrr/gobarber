780adc90af0a5fc379681e28032dcb16669d8cfd
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2020-08-08 01:28:24 -0300

    Iniciando o back-end

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-param-reassign': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // 11DeD$_@_ddnj@D._>
+  secret: '6d230a95dfb43e7b5de11b17a354e6b8',
+  expiresIn: '7d',
+};
