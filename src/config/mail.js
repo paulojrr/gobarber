44700a40f8374eeca3cf31ac145f2708d578771f
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2020-08-15 23:30:31 -0300

    Marcar notificações como lidas, Cancelamento de agendamento, Configurando Nodemailer,  Configurando templates de e-mail, Configurando fila com Redis, Monitorando falhas na fila

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e4bac72a97bdf3',
+    pass: 'eda22d4c43d7ed',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
